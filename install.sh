042d9c493e815000175ebfcce1d30869dab9ddf1
Robus Gauli <robusgauli@gmail.com>
2020-10-19 21:44:22 +0545

    Fix the root exec

--- install.sh
+++ install.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 
-if [[ `uname` = "Darwin" ]]; then
-  BIN=/usr/local/bin
-  CURL=curl
-else
-  # check if user is root
-  # and re-exec the script with sudo if not
-  #
-  # equivalent to: [ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
-  # bash only, but without external call to whoami
-  #
-  (( EUID != 0 )) && exec sudo -- "$0" "$@"
-  BIN=/usr/bin
-  CURL="sudo curl"
+if [[ "$EUID" -ne 0 ]]; then
+  exec sudo -- "$0" "$@"
 fi
 
-echo "Installing with"
-$CURL -s https://raw.githubusercontent.com/euclideang/mx.sh/master/mx.sh -o $BIN/mx && chmod +x $BIN/mx
-echo "with successfully installed!"
+if [[ $(uname) -eq "Darwin" ]]; then
+  BIN=/usr/local/bin
+else
+  BIN=/usr/bin
+fi
+
+curl -s https://raw.githubusercontent.com/euclideang/mx.sh/master/mx.sh -o $BIN/mx && chmod +x $BIN/mx
+echo "Installation complete. Type 'mx' to learn more."
