7c0a794e1af65ee0bb0321502f1de41f5f3be517
Robus Gauli <robusgauli@gmail.com>
2020-10-26 11:04:15 +0545

    Add version and configured installation script to use versioned script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+VERSION=$(cat version)
+
 if [[ "$EUID" -ne 0 ]]; then
   exec sudo -- "$0" "$@"
 fi
@@ -10,5 +12,5 @@ else
   BIN=/usr/bin
 fi
 
-curl -s https://raw.githubusercontent.com/euclideang/mx.sh/master/mx.sh -o $BIN/mx && chmod +x $BIN/mx
+curl -s https://raw.githubusercontent.com/euclideang/mx.sh/"${VERSION}"/mx.sh -o ${BIN}/mx && chmod +x ${BIN}/mx
 echo "Installation complete. Type 'mx' to learn more."
